3699fdedb0415b9ee8fafab456168b258b33a665
Emirsyah <muhammademir48@gmail.com>
2022-05-25 22:49:18 +0700

    sign up pages added

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import logo from "../assets/merchainLogo.svg";
 import GoogleLogin from "../components/GoogleLogin";
 
-function Login() {
+function Signup() {
   return (
     <>
       <nav className="py-6 px-16 flex items-center justify-between ">
@@ -11,39 +11,47 @@ function Login() {
           <img src={logo} alt="merchain icon" />
         </Link>
         <div>
-          <span className="opacity-80">Belum punya akun? </span>
+          <span className="opacity-80">Sudah punya akun? </span>
           <Link
-            to="/signup"
+            to="/login"
             className="font-semibold text-purple-600 underline hover:font-bold"
           >
-            Daftar
+            Masuk
           </Link>
         </div>
       </nav>
       <div className="flex justify-center mt-6 flex-col max-w-sm mx-auto">
-        <h1 className="text-3xl font-semibold text-center">
-          Selamat Datang Kembali!
-        </h1>
+        <h1 className="text-3xl font-semibold text-center">Daftar Sekarang</h1>
 
         {/* Form Login Biasa */}
         <form className="flex flex-col mt-8 gap-3">
+          {/* Input nama toko */}
+          <div className="font-medium p-4 flex py-3 border-2 border-gray-300 rounded-lg outline-purple-500 hover:border-purple-600 duration-500 ease-out transition-all">
+            <p>merchain.com/</p>
+            <input
+              type="text"
+              required
+              placeholder="tokokamu"
+              className="required outline-none"
+            />
+          </div>
           <input
             type="text"
-            placeholder="Email"
             required
+            placeholder="Email"
             className="required font-medium p-4 py-3 border-2 border-gray-300 rounded-lg outline-purple-500 hover:border-purple-600 duration-500 ease-out transition-all"
           />
           <input
             type="password"
-            placeholder="Password"
             required
+            placeholder="Password"
             className="required font-medium p-4 py-3 border-2 border-gray-300 rounded-lg outline-purple-500 hover:border-purple-600 duration-500 ease-out transition-all"
           />
           <button
             type="submit"
             className="p-3 mt-2 tracking-widest font-semibold hover:bg-purple-700 transition-all duration-200 ease-out bg-purple-600 text-white rounded-lg"
           >
-            Masuk Sekarang
+            Daftar
           </button>
         </form>
         <p class="text-center overflow-hidden before:h-[1.5px] font-medium opacity-60 after:h-[1.5px] after:bg-slate-400 after:inline-block after:relative after:align-middle after:w-1/4 before:bg-slate-400 before:inline-block before:relative before:align-middle before:w-1/4 before:right-2 after:left-2  p-8">
@@ -57,4 +65,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Signup;

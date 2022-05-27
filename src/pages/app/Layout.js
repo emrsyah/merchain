fa60f5fc0de01d1638c245702b2d1eb1173946b1
Emirsyah <muhammademir48@gmail.com>
2022-05-27 08:32:36 +0700

    get user & store data when first load at /app

--- src/pages/app/Layout.js
+++ src/pages/app/Layout.js
@@ -1,8 +1,53 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useSetRecoilState } from "recoil";
+import { userState } from "../../atoms/userAtom";
 import Sidebar from "../../components/Sidebar";
+import { auth, firestoreDb } from "../../firebase";
 
 function Layout() {
+  const navigate = useNavigate();
+  const setUser = useSetRecoilState(userState);
+  const [store, setStore] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchAndSetStore = (uid) => {
+    onSnapshot(
+      query(collection(firestoreDb, "spaces"), where("userId", "==", uid)),
+      (snapshot) => {
+        setStore(snapshot.docs);
+      }
+    );
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    try {
+      onAuthStateChanged(auth, (user) => {
+        if (!user) {
+          navigate("/login");
+          toast.info("Tolong login dahulu");
+        }
+        fetchAndSetStore(user.uid);
+        setUser({ uid: user.uid });
+        console.log(user)
+      });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  if(loading){
+    return(
+      <div>Loading...</div>
+    )
+  }
+
   return (
     <div className="grid grid-cols-11">
       <nav className="col-span-2">

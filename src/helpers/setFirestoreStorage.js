8dc3a64f9729321559bccbdb72e94ea5221d5267
Emirsyah <muhammademir48@gmail.com>
2022-06-06 18:10:20 +0700

    refactor setFirestoreStorage helper function

--- src/helpers/setFirestoreStorage.js
+++ src/helpers/setFirestoreStorage.js
@@ -1,9 +1,8 @@
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { storage } from "../firebase";
 
-export default async function setFirestoreStorage(img, id){
-    console.log(id)
-  const storageRef = ref(storage, `stores-profile/${id}`);
+export default async function setFirestoreStorage(img, id, col){
+  const storageRef = ref(storage, `${col}/${id}`);
   const uploadTask = await uploadBytesResumable(storageRef, img);
   const url = await getDownloadURL(uploadTask.ref);
   return url;

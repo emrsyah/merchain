d5d27c68c3d1d7ae9afb11016160d3b938a0401f
Emirsyah <muhammademir48@gmail.com>
2022-06-18 21:02:21 +0700

    cart img size fixed, no product in cart ui added

--- src/components/NavbarStore.js
+++ src/components/NavbarStore.js
@@ -17,9 +17,9 @@ function NavbarStore() {
         <img src={logo} alt="merchain logo" className="w-3/4 md:w-auto" />
       </Link>
       <div className="flex items-center justify-between gap-2 md:gap-10">
-        <Link to="/signup" className={` ${color + "Nav"} font-medium text-xs md:text-base transition-all duration-150 ease-out hover:-translate-y-[2px]  py-2 px-4 rounded-md md:rounded-lg border-[1.4px] cursor-pointer`}>Coba Merchain</Link>
+        <Link to="/signup" className={` ${color? color + "Nav" : "purpleNav"} font-medium text-xs md:text-base transition-all duration-150 ease-out hover:-translate-y-[2px]  py-2 px-4 rounded-md md:rounded-lg border-[1.4px] cursor-pointer`}>Coba Merchain</Link>
         <div className="relative cursor-pointer" onClick={()=>navigate('/cart')}>
-            <div className={` ${color + "-btn"} w-5 h-5 rounded-full absolute -right-[10px] -top-2 text-xs p-2 text-white flex items-center justify-center`}>{count}</div>
+            <div className={` ${color? color + "-btn" : "purple-btn"} w-5 h-5 rounded-full absolute -right-[10px] -top-2 text-xs p-2 text-white flex items-center justify-center`}>{count}</div>
             <Icon icon="clarity:shopping-bag-line" width={28}/>
         </div>
       </div>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,17 +2,21 @@ import React from "react";
 import { useEffect } from "react";
 import { Helmet } from "react-helmet-async";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { cartState, cartTotal } from "../atoms/cartAtom";
+import { cartCount, cartState, cartTotal } from "../atoms/cartAtom";
 import { storeColor } from "../atoms/storeColor";
 import CartItem from "../components/CartItem";
 import NavbarStore from "../components/NavbarStore";
 import { deleteFromCart } from "../helpers/helperCart";
 import rupiahConverter from "../helpers/rupiahConverter";
+import noCart from "../assets/noCart.svg";
+import { useNavigate } from "react-router-dom";
 
 function Cart() {
   const [cart, setCart] = useRecoilState(cartState);
   const color = useRecoilValue(storeColor);
-  const total = useRecoilValue(cartTotal)
+  const total = useRecoilValue(cartTotal);
+  const count = useRecoilValue(cartCount)
+  const navigate = useNavigate();
 
   useEffect(() => {
     console.log(cart);
@@ -30,33 +34,62 @@ function Cart() {
       </Helmet>
       <NavbarStore />
       <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-5 my-6 gap-2 md:gap-6 poppins">
-        <div className="col-span-3">
-          <h5 className="text-xl font-semibold">Belanjaan</h5>
-          <div className="border-[1.2px] border-gray-200 mt-[10px] px-3 py-4 rounded-md flex flex-col gap-4">
-            {cart.map((c) => (
-              <CartItem
-                image={c.product.image}
-                name={c.product.name}
-                price={c.product.price}
-                quantity={c.quantity}
-                id={c.id}
-                key={c.id}
-                deleteHandler={deleteHandler}
-                color={color}
-              />
-            ))}
-          </div>
-        </div>
-
-        <div className=" col-span-2">
-          <h5 className="text-xl font-semibold">Pembayaran</h5>
-          <div className="border-[1.2px] mt-[10px] border-gray-200 p-2 rounded-md">
-            <div className="flex justify-between items-center">
-              <p className="text-gray-600 font-medium">Total</p>
-              <p className={`font-semibold ${color + "-txt"} `}>{rupiahConverter(total)}</p>
+        {cart.length > 0 ? (
+          <>
+            <div className="col-span-3">
+              <h5 className="text-xl font-semibold">Belanjaan</h5>
+              <div className="border-[1.2px] border-gray-200 mt-[10px] px-3 py-4 rounded-md flex flex-col gap-4">
+                {cart.map((c) => (
+                  <CartItem
+                    image={c.product.image}
+                    name={c.product.name}
+                    price={c.product.price}
+                    quantity={c.quantity}
+                    id={c.id}
+                    key={c.id}
+                    deleteHandler={deleteHandler}
+                    color={color}
+                  />
+                ))}
+              </div>
             </div>
+
+            <div className=" col-span-2">
+              <h5 className="text-xl font-semibold">Pembayaran</h5>
+              <div className="border-[1.2px] mt-[10px] flex flex-col gap-2 border-gray-200 p-2 rounded-md">
+                <div className="flex justify-between items-center">
+                  <p className="text-gray-600 font-medium">Total</p>
+                  <p className={`font-semibold ${color + "-txt"} `}>
+                    {rupiahConverter(total)}
+                  </p>
+                </div>
+                <div className="flex justify-between items-center">
+                  <p className="text-gray-600 font-medium">Jumlah Item Dibeli</p>
+                  <p className={`font-semibold ${color + "-txt"} `}>
+                    {count}
+                  </p>
+                </div>
+                  <button className="font-semibold mt-3 bg-purple-600 hover:bg-purple-700 rounded text-white p-3">Checkout</button>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="col-span-5 flex flex-col items-center">
+            <img src={noCart} className="w-[360px]" alt="noCartImg" />
+            <h5 className="font-semibold text-2xl">
+              Keranjang Kamu Masih Kosong
+            </h5>
+            <p className="text-gray-600 mt-1">
+              Silahkan kembali setelah memilih produk
+            </p>
+            <button
+              className="font-medium text-lg py-2 px-16 hover:bg-purple-700 rounded mt-4 bg-purple-600 text-white"
+              onClick={() => navigate(-1)}
+            >
+              Kembali
+            </button>
           </div>
-        </div>
+        )}
       </div>
     </>
   );
